d0132fd22759d2cf9280283ea99fecdc23e3f392
ahmadabusarhan5-crypto <ahmadabusarhan5@gmail.com>
2026-06-09 20:23:20 +0300

    Update factors.py

--- factors.py
+++ factors.py
@@ -1,6 +1,19 @@
 def factors(x):
-    f_list = []
-    for i in range(1, x + 1):
-        if x % i == 0:
-            f_list.append(i)
-    return f_list
+  f_list = []
+  for i in range(1, x + 1):
+    if x % i == 0:
+      f_list.append(i)
+  return f_list
+if __name__ == "__main__":
+    try:
+        # קריאת הקלט מהבודק האוטומטי
+        user_input = int(input())
+        
+        # חישוב התוצאה
+        result = factors(user_input)
+        
+        # הדפסת התוצאה כפי שהבודק מצפה לראות (רשימה)
+        print(result)
+    except EOFError:
+        # טיפול במקרה שהקלט ריק
+        pass
