5e126b77f582b5d23755354451f81fb9ade450d0
ahmadabusarhan5-crypto <ahmadabusarhan5@gmail.com>
2026-06-09 19:56:43 +0300

    Update factors.py

--- factors.py
+++ factors.py
@@ -1,3 +1,13 @@
 def factors(x):
-  # your code here
-  return a_list
+def factors(x):
+  f_list = []
+  for i in range(1, x + 1):
+    if x % i == 0:
+      f_list.append(i)
+
+  return f_list
+#check your code
+print(factors(6))
+print(factors(17))
+print(factors(24))
+print(factors(48))
